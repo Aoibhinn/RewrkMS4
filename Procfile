e6f2f40a677e2d755327293e02b2e6704730ff45
Aoibhinn <97aoibhinn@gmail.com>
2021-11-05 22:05:54 +0000

    create case study model

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:gunicorn rewrk.wsgi
+web: gunicorn rewrk.wsgi
--- casestudy/models.py
+++ casestudy/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Post(models.Model):
+    employee = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250, unique=True)
+    service = models.IntegerField()
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
