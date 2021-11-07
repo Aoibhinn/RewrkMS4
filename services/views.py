5dd4157f3cd1d206b6dd91d2e8ccb7e2d419cc80
Aoibhinn <97aoibhinn@gmail.com>
2021-11-07 11:26:17 +0000

    create view code for services

--- services/views.py
+++ services/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django .views import generic
+from .models import Service
 
-# Create your views here.
+Class ServiceList(generic.ListView):
+    model = Service
+    queryset = Course.objects.order_by('plan').filter(is_published=True)
+    context_object_name = "services"
+    paginate_by = 6
+
+
