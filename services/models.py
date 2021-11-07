01b7f4f05333143f0e21d1507bfbd2945c766d2e
Aoibhinn <97aoibhinn@gmail.com>
2021-11-07 10:00:51 +0000

    create model for services model.py

--- services/models.py
+++ services/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+PLAN_CHOICES = (
+    ('B', 'basic'),
+    ('S', 'standard'),
+    ('P', 'premium')
+)
+
+class Service(models.Model):
+    employee = models.ForeignKey(User, on_delete=models.CASCADE)
+    serice_title = models.CharField(max_length=250)
+    service_description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    plan = models.CharField(choices=PLAN_CHOICES,max_length=1)
+    is_published = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return self.title
+
+
+
